66d9c71861d76e5d4287fe57b11505ae5b1c5b1e
ncpa0cpl <sbretner@touchbistro.com>
2022-10-03 12:06:52 +0200

    added dist to git tree

---
+++ dist/jsxte/legacy/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/jsxte/index.ts
+var jsxte_exports = {};
+__export(jsxte_exports, {
+  Link: () => import_link.Link,
+  WebFrame: () => import_web_frame.WebFrame,
+  registerFrameView: () => import_register_frame_view.registerFrameView
+});
+module.exports = __toCommonJS(jsxte_exports);
+var import_link = require("./components/link.js");
+var import_web_frame = require("./components/web-frame.js");
+var import_register_frame_view = require("./register-frame-view.js");
